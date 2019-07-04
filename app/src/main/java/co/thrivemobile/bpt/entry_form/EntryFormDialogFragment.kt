00c407e6e1d641f4f27fda85a3ffe6113eb92e26
Alan Hill <hill.e.alan@gmail.com>
2019-07-03 20:55:25 -0400

    Complete BottomSheet Styling
     * complete the styling of the chips and buttons for the entry from bottom sheet

--- app/src/main/java/co/thrivemobile/bpt/entry_form/EntryFormDialogFragment.kt
+++ app/src/main/java/co/thrivemobile/bpt/entry_form/EntryFormDialogFragment.kt
@@ -1,15 +1,19 @@
 package co.thrivemobile.bpt.entry_form
 
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Xml
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.StringRes
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.lifecycle.Observer
 import co.thrivemobile.bpt.R
 import co.thrivemobile.bpt.base.BaseBottomDialogFragment
 import co.thrivemobile.bpt.databinding.DialogEntryFormBinding
 import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
 import com.google.android.material.chip.ChipGroup
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -47,6 +51,7 @@ class EntryFormDialogFragment : BaseBottomDialogFragment() {
     private fun setUpChips(chipGroup: ChipGroup) {
         for (i in 1..10) {
             val chip = Chip(context).apply {
+                setChipDrawable(ChipDrawable.createFromResource(context, R.xml.chip_styling))
                 text = i.toString()
                 id = i
                 setTextAppearanceResource(R.style.ChipTextStyle)
