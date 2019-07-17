4667bbb05320383c513eabe238ba4d75f6748826
Alan Hill <hill.e.alan@gmail.com>
2019-07-17 10:37:57 -0500

    Info Item Layouts
     * create all the layouts for the info screen items
     * set up the ViewHolders to using the bindings

--- app/src/main/java/co/thrivemobile/bpt/info/vh/ArticleViewHolder.kt
+++ app/src/main/java/co/thrivemobile/bpt/info/vh/ArticleViewHolder.kt
@@ -1,10 +1,12 @@
 package co.thrivemobile.bpt.info.vh
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import co.thrivemobile.bpt.databinding.ItemArticleBinding
 import co.thrivemobile.bpt.info.items.ArticleItem
 
-class ArticleViewHolder(root: View) : RecyclerView.ViewHolder(root), InfoViewHolder<ArticleItem> {
+class ArticleViewHolder(
+    private val binding: ItemArticleBinding
+) : RecyclerView.ViewHolder(binding.root), InfoViewHolder<ArticleItem> {
     override fun bind(item: ArticleItem) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
--- app/src/main/java/co/thrivemobile/bpt/info/vh/WhatIsViewHolder.kt
+++ app/src/main/java/co/thrivemobile/bpt/info/vh/WhatIsViewHolder.kt
@@ -1,10 +1,12 @@
 package co.thrivemobile.bpt.info.vh
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import co.thrivemobile.bpt.databinding.ItemWhatIsBinding
 import co.thrivemobile.bpt.info.items.WhatIsItem
 
-class WhatIsViewHolder(root: View) : RecyclerView.ViewHolder(root), InfoViewHolder<WhatIsItem> {
+class WhatIsViewHolder(
+    private val binding: ItemWhatIsBinding
+) : RecyclerView.ViewHolder(binding.root), InfoViewHolder<WhatIsItem> {
     override fun bind(item: WhatIsItem) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
