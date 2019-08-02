af6b86a383162904601beafa5daabc710a743bd1
Alan Hill <hill.e.alan@gmail.com>
2019-08-02 11:47:45 -0400

    Network Testing
     * added a simple test to make sure an empty metadata object comes back
       with an empty response

--- networking/src/test/java/co/thrivemobile/networking/NetworkTest.kt
+++ networking/src/test/java/co/thrivemobile/networking/NetworkTest.kt
@@ -1,28 +1,52 @@
 package co.thrivemobile.networking
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.Call
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 
-@ExtendWith(value = [ MockitoExtension::class ])
+@ExperimentalCoroutinesApi
+@ExtendWith(value = [ MockitoExtension::class ] )
 class NetworkTest {
 
     @Mock lateinit var mockClient: OkHttpClient
-    @ExperimentalCoroutinesApi private val testDispatcher = Dispatchers.Unconfined
-    @ExperimentalCoroutinesApi private val network = Network(
-        testDispatcher,
-        testDispatcher,
-        mockClient
-    )
+    private val testDispatcher = Dispatchers.Unconfined
+    private lateinit var network: Network
 
     @Test
     @DisplayName("When request comes back empty then we get default metadata")
     fun emptyNetwork() {
-        
+        network = Network(
+            testDispatcher,
+            testDispatcher,
+            mockClient
+        )
+        val expectedMetaData = MetaData()
+
+        val mockResponse: Response = mock()
+        whenever(mockResponse.body).thenAnswer { "".toResponseBody() }
+
+        val mockCall: Call = mock()
+        whenever(mockCall.execute()).thenAnswer {
+            mockResponse
+        }
+
+        whenever(mockClient.newCall(any())).thenAnswer {
+            mockCall
+        }
+        network.getMetaData("https://www.dummyurl.com") {
+            assertTrue(it == expectedMetaData)
+        }
     }
 }
